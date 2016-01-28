17bfe3e8939da2e7e22d1a01a98ef12ff1f55c17
Ben Wu <ben@pelotoncycle.com>
2016-01-27 22:39:24 -0500

    additional changes

--- webapp/chef-repo/cookbooks/awesome_customers/attributes/default.rb
+++ webapp/chef-repo/cookbooks/awesome_customers/attributes/default.rb
@@ -3,3 +3,13 @@ default['awesome_customers']['group'] = 'web_admin'
 
 default['awesome_customers']['document_root'] = '/var/www/customers/public_html'
 default['firewall']['allow_ssh'] = true
+
+default['awesome_customers']['passwords']['secret_path'] = '/etc/chef/encrypted_data_bag_secret'
+
+default['awesome_customers']['database']['dbname'] = 'products'
+default['awesome_customers']['database']['host'] = '127.0.0.1'
+default['awesome_customers']['database']['username'] = 'root'
+
+default['awesome_customers']['database']['app']['username'] = 'db_admin'
+
+default['awesome_customers']['database']['seed_file'] ='/tmp/create-tables.sql'
--- webapp/chef-repo/cookbooks/awesome_customers/recipes/webserver.rb
+++ webapp/chef-repo/cookbooks/awesome_customers/recipes/webserver.rb
@@ -14,7 +14,7 @@ end
 httpd_config 'customers' do
   instance 'customers'
   source 'customers.conf.erb'
-  notifies :restart, 'http_service[customers]'
+  notifies :restart, 'httpd_service[customers]'
 end
 
 # Create the document root dir
@@ -22,12 +22,19 @@ directory node['awesome_customers']['document_root'] do
   recursive true
 end
 
+# Load the secrets file and the encrypted data bag item that holds the database password.
+password_secret = Chef::EncryptedDataBagItem.load_secret(node['awesome_customers']['passwords']['secret_path'])
+user_password_data_bag_item = Chef::EncryptedDataBagItem.load('passwords', 'db_admin_password', password_secret)
+
 # Write a default home page.
-file "#{node['awesome_customers']['document_root']}/index.php' do
-  content '<html>This is a place holder</html>'
+template "#{node['awesome_customers']['document_root']}/index.php" do
+  source 'index.php.erb'
   mode '0644'
   owner node['awesome_customers']['user']
   group node['awesome_customers']['group']
+  variables({
+    :database_password => user_password_data_bag_item['password']
+  })
 end
 
 # Open port 80 to incoming traffic.
@@ -37,3 +44,13 @@ firewall_rule 'http' do
   action :allow
 end
 
+# Install the mod_php5 Apache module.
+httpd_module 'php5' do
+  instance 'customers'
+end
+
+# Install php5-mysql.
+package 'php5-mysql' do
+  action :install
+  notifies :restart, 'httpd_service[customers]'
+end
